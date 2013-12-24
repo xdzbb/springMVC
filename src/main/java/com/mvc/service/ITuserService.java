465b2d778ff7b2d3eb82e4836b5fecfb203ce036
bbsyj <345129564@qq.com>
2013-12-24 16:05:16 +0800

    UserService

--- src/main/java/com/mvc/service/ITuserService.java
+++ src/main/java/com/mvc/service/ITuserService.java
@@ -1,6 +1,45 @@
 package com.mvc.service;
 
+import java.util.List;
+import com.mvc.entity.Pager;
+import com.mvc.entity.Tuser;
+
 public interface ITuserService {
 
+	/**
+	 * 添加
+	 * @param t
+	 * @return
+	 */
+	public int saveUser(Tuser user);
+	
+	/**
+	 * 更新
+	 * @param t
+	 * @return
+	 */
+	public int updateUser(Tuser user);
+	
+	/**
+	 * 删除
+	 * @param t
+	 * @return
+	 */
+	public int deleteUser(Tuser user); 
+	
+	/**
+	 * 离线式组合查询（带分页）
+	 * @param dc
+	 * @param pager
+	 * @return
+	 */
+	public List<Tuser> findByDetach(Tuser tuser,Pager pager);
+	
+	/**
+	 * 查询总记录个数
+	 * @param dc
+	 * @return
+	 */
+	public Integer findByConut(Tuser tuser);
 
 }
--- src/main/java/com/mvc/service/impl/TuserServiceImpl.java
+++ src/main/java/com/mvc/service/impl/TuserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.mvc.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+
+import org.hibernate.criterion.DetachedCriteria;
 import org.springframework.stereotype.Service;
 
 import com.mvc.dao.impl.BaseDaoImpl;
 import com.mvc.entity.Articletype;
+import com.mvc.entity.Pager;
 import com.mvc.entity.Tuser;
 import com.mvc.service.ITuserService;
 
@@ -13,4 +18,36 @@ public class TuserServiceImpl implements ITuserService {
 
 	@Resource
 	private BaseDaoImpl baseDao;
+	
+	@Override
+	public int deleteUser(Tuser user) {
+		// TODO Auto-generated method stub
+		return baseDao.deleteObject(user);
+	}
+	
+	@Override
+	public Integer findByConut(Tuser tuser) {
+		// TODO Auto-generated method stub
+		DetachedCriteria dc=DetachedCriteria.forClass(Tuser.class);
+		return baseDao.findByConut(dc);
+	}
+	
+	@Override
+	public List<Tuser> findByDetach(Tuser tuser, Pager pager) {
+		// TODO Auto-generated method stub
+		DetachedCriteria dc=DetachedCriteria.forClass(Tuser.class);
+		return baseDao.findByDetach(dc, pager);
+	}
+	
+	@Override
+	public int saveUser(Tuser user) {
+		// TODO Auto-generated method stub
+		return baseDao.saveObject(user);
+	}
+	
+	@Override
+	public int updateUser(Tuser user) {
+		// TODO Auto-generated method stub
+		return baseDao.saveObject(user);
+	}
 }
